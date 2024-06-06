83ae4f602331e5a9a83f2360e4a1df13b8c97e03
Morceline <pereirajackeline988@gmail.com>
2024-06-06 20:06:36 -0300

    Add files via upload

---
+++ prova-erm/pratico/p001/script.js
@@ -0,0 +1,73 @@
+
+
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/style2/main.css',
+  '/style/main.css',
+  '/script/main.js',
+  '/imagens/foto-gojo.jpeg',
+  '/imagens/foto-onepiece.jpeg',
+  '/imagens/foto-nezuko.jpeg'
+];
+
+// Instalando o service worker e abrindo o cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativando o service worker e limpando caches antigos, se houver
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptando as requisições e respondendo a partir do cache, se possível
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Cache miss - fetch from network
+        return fetch(event.request).then(
+          networkResponse => {
+            // Check if we received a valid response
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+
+            // Clone the response
+            const responseToCache = networkResponse.clone();
+
+            // Open the cache and store the response
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
+
